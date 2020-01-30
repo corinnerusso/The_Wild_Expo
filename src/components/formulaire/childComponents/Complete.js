807017bc08fa40d7fe3c913d263e80f3734cdfcf
Corinne Russo <corinne.russo@gmail.com>
2020-01-30 17:16:40 +0100

    more css

--- src/components/formulaire/childComponents/Complete.js
+++ src/components/formulaire/childComponents/Complete.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import "./complete.css";
@@ -8,14 +8,29 @@ const Complete = () => {
     firstname: "",
     lastname: "",
     email: "",
-    nb_visitors: "",
-    date_resa: ""
+    nb_visitors: 0,
+    date_resa: "",
+    nb_children: 0,
+    price_to_pay: 0
   });
 
   //update form with inputs
   const updateForm = e => {
     setForm({ ...form, [e.target.name]: e.target.value });
     console.log("form : ", form);
+  };
+
+  const handleTotal = () => {
+    let total = 0;
+
+    if (form.nb_visitors || form.nb_children) {
+      total = form.nb_visitors * 10 + form.nb_children * 5;
+    }
+    if (form.nb_visitors == "group") {
+      total = 25;
+    }
+
+    return total;
   };
 
   //submit infos on database on submit
@@ -26,7 +41,9 @@ const Complete = () => {
         lastname: form.lastname,
         email: form.email,
         nb_visitors: form.nb_visitors,
-        date_resa: form.date_resa
+        date_resa: form.date_resa,
+        nb_children: form.nb_children,
+        price_to_pay: handleTotal()
       })
       .then(function(response) {
         setForm({
@@ -34,7 +51,9 @@ const Complete = () => {
           lastname: "",
           email: "",
           nb_visitors: "",
-          date_resa: ""
+          date_resa: "",
+          nb_children: "",
+          price_to_pay: ""
         });
       })
       .catch(function(error) {});
@@ -48,7 +67,6 @@ const Complete = () => {
       <div className="form_inputs">
         {/* nom */}
         <input
-          // ref={inputEl}
           className="addUser-input"
           type="text"
           name="lastname"
@@ -57,7 +75,6 @@ const Complete = () => {
         />
         {/* prénom */}
         <input
-          // ref={inputElaa}
           className="addUser-input"
           type="text"
           name="firstname"
@@ -66,22 +83,31 @@ const Complete = () => {
         />
         {/* email */}
         <input
-          // ref={inputElbb}
           className="addUser-input"
           type="text"
           name="email"
           placeholder="Email"
           onChange={updateForm}
         />
-        <label for="place-select">Nombre de places</label>
+        <label for="place-select">Nombre de places adultes</label>
         <select name="nb_visitors" onChange={updateForm}>
           <option value="">--Faites votre choix--</option>
           <option value="one">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
-          <option value="groupe">Groupe (+ 5 pers)</option>
+          <option value="group">5 personnes et plus</option>
         </select>
+
+        <label for="place-select">Nombre de places enfants</label>
+        <select name="nb_children" onChange={updateForm}>
+          <option value="">--Faites votre choix--</option>
+          <option value="one">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+
         <label for="artist-select">Date de la réservation</label>
         <input
           type="date"
@@ -89,7 +115,7 @@ const Complete = () => {
           name="date_resa"
           onChange={updateForm}
         />
-        <div className="complete_date">Prix total : </div>
+        <div className="complete_date">Total : {handleTotal()} € </div>
         <button text="Valider" onClick={() => submitForm()}>
           Valider
         </button>
